c764d5d87b7f45e1b229e6067208399e082ec86e
Itzzakshat <83529349+Itzzakshat@users.noreply.github.com>
2022-09-21 02:01:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Wheat from "./Wheat";
+function App() {
+  return (
+    <div className="App">
+      <Wheat />
+    </div>
+  );
+}
+
+export default App;
